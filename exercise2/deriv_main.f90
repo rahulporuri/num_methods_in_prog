03b4ab059f313e77d07f140647e7588e94f76b93
rahul.poruri <rahul.poruri@gmail.com>
2015-02-07 22:25:11 +0530

    derivative in fortran

---
+++ exercise2/deriv_main.f90
@@ -0,0 +1,20 @@
+	PROGRAM deriv_main
+	implicit none
+	double precision, parameter :: x = 0, h_min = 1e-16, h_max = 1e-01
+	double precision :: deriv, error,  h
+	h = h_max
+10	if (h .gt. h_min) then
+	h = h/2
+	deriv = (exp(x+h)-exp(x-h))/(2*h)
+	error = deriv - exp(x)
+!	print *, 'the product of the two numbers is:', deriv
+!	print *, 'the error in derivative is:', error
+	write(10,*) h, deriv, error
+	goto 10
+	end if
+	END
+
+
+
+
+
