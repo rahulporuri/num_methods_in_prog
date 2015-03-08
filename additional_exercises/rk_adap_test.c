9301c51c2820485be87a1c9e0efd0ca9c47e6165
rahul.poruri <rahul.poruri@gmail.com>
2015-03-08 10:42:26 +0530

    working adaptive step size rk4/5 code with subroutines

--- additional_exercises/rk_adap_test.c
+++ additional_exercises/rk_adap_test.c
@@ -3,31 +3,45 @@
 #include <math.h>
 
 double func(double, double);
-void rk4_step(double *, double);
+void rk_adap_step(double, double *, double);
 
 int
 main(void)
 {
 	double h = 0.1;
+	double x = 0.;
+	double err;
+	double tol = 0.1;
+	double g = 1./2;
 
 	double *array;
 	array = (double*)malloc(2*sizeof(double));
-	double *array_test;
-	array_test = (double*)malloc(2*sizeof(double));
 
 	int i;
 
 	FILE *in_ptr;
 	in_ptr = fopen("rk_adap_test.dat","w");
 
-	array[0] = 0;
+	array[0] = 0.5;
 	array[1] = 0.5;
 
 	for (i=0;i<100;i++)
 	{
-		rk4_step(array,h);
-//		printf("%lf, %lf \n", x,y);
-		fprintf(in_ptr, "%lf, %lf \n", array[0],array[1]);
+		rk_adap_step(x, array,h);
+		err = fabs(array[0]-array[1]);
+//		printf("%lf, %lf \n", x, err);
+		if (err > tol)
+		{
+			h = h*pow(g*tol/err, 1./5);
+//			printf("redo step! \n");
+		}
+//		printf("%lf, %lf \n", x, err);
+//		printf("%lf, %lf, %lf \n", x, array[0], array[1]);
+		else
+		{
+			fprintf(in_ptr, "%lf, %lf, %lf \n", x, array[0], array[1]);
+			x += h;
+		}
 	}
 
 	fclose(in_ptr);
@@ -39,21 +53,22 @@ double func(double x, double y)
 	return (10*exp(-pow(x-2,2)/(2*pow(0.075,2))) - 0.6*y);
 }
 
-void rk4_step(double *array, double h)
+void rk_adap_step(double x , double *array, double h)
 {
-	double k1, k2, k3, k4;
-	double x,y;
+	double k1, k2, k3, k4, k5, k6;
+	double y;
 
-	x = array[0];
-	y = array[1];
+	y = array[0];
 
 	k1 = (*func)(x,y);
-	k2 = (*func)(x+h/2., y+k1*h/2.);
-	k3 = (*func)(x+h/2., y+k2*h/2.);
-	k4 = (*func)(x+h,y+k3*h);
+	k2 = (*func)(x +h/5., y+k1*h/5.);
+	k3 = (*func)(x +3*h/10., y +(3*k1/40. +9*k2/40.)*h);
+	k4 = (*func)(x +3*h/5., y +(3*k1/10 -9*k2/10. + 6*k3/5)*h);
+	k5 = (*func)(x +h, y +(-11*k1/54 +5*k2/2. -70*k3/27 +35*k4/27)*h);
+	k6 = (*func)(x +7*h/8., y +(1631*k1/55296 +175*k2/512 + 575*k3/13824 +44275*k4/110592 +253*k5/4096)*h);
 
-	array[0] += h;
-	array[1] += (k1 +2*k2 +2*k3 +k4)*h/6.;
+	array[0] = array[0] + (37*k1/378. +250*k3/621. +125*k4/594. +512*k6/1771.)*h;
+	array[1] = array[0] + (2825*k1/27648. +12575*k3/48384. +13525*k4/55296. +277*k5/14336. +k6/4.)*h;
 
 	return;
 }
