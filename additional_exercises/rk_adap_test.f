af1eceda01c84a4fc925d1089a9b3361e9801374
rahul.poruri <rahul.poruri@gmail.com>
2015-03-08 13:50:24 +0530

    rk adaptive working

--- additional_exercises/rk_adap_test.f
+++ additional_exercises/rk_adap_test.f
@@ -3,12 +3,16 @@
 	real(kind=4) :: x,y,yt,h
 	real(kind=4) :: k1, k2, k3, k4, k5, k6, temp1, temp2
 	real(kind=4) :: temp3
+	real(kind=4) :: tol, err, g
 	integer :: i
+	g = 5e-01
+	tol = 1e-01
 	i = 0
 	x = 0
 	y = 5e-01
 	h = 1e-01
 10	if (i .le. 100) then
+
 	k1 = func(x,y)
 	k2 = func(x +h/5, y+k1*h/5)
 	k3 = func(x +3*h/10, y +3*k1*h/40 +9*k2*h/40)
@@ -21,12 +25,22 @@
 	y = y + (37*k1/378 +250*k3/621 +125*k4/594 +512*k6/1771)*h;
 	temp3 = 13525*k4/55296+277*k5/14336+k6/4
 	yt=y+(2825*k1/27648+12575*k3/48384+temp3)*h
-		x = x +h
-!		print *, x
-!		print *, abs(y-yt)
-		write(10,*) x,y,yt
-		i = i +1
-		goto 10
+	err = abs(y-yt)
+	print *, err
+	if (err .ge. tol) then
+	h = h*(g*tol/err)**(1./5)
+	print *, 'redo step'
+	end if
+
+	if (err .le. tol) then
+	x = x +h
+	write(10,*) x,y,yt
+	end if
+!	print *, x
+!	print *, abs(y-yt)
+!	write(10,*) x,y,yt
+	i = i +1
+	goto 10
 	end if
 	end
 
