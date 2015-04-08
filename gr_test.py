d68f080b01982d39db37bfd0f91b4251dc16e107
rahul.poruri <rahul.poruri@gmail.com>
2015-04-08 12:57:35 +0530

    cleanup

---
+++ gr_test.py
@@ -0,0 +1,44 @@
+import numpy as np
+import scipy.linalg as spalg
+import matplotlib.pyplot as plt
+
+a = 0.
+b = 1.
+N = 100
+
+A = np.zeros([N,N])
+B = np.zeros(N)
+U = np.zeros(N)
+
+h = (b-a)/(N-1)
+print h
+
+def fn(x):
+	return (3*x+x**2)*np.exp(x)
+
+def un(x):
+	return x*(1-x)*np.exp(x)
+
+for i in range(len(A)):
+	for j in range(len(A[0])):
+		# loading A
+		if i==j:
+			A[i][j] = 2
+		if abs(i-j) == 1:
+			A[i][j] = -1
+	B[i] = h**2*fn(a+i*h)
+	U[i] = un(a+i*h)
+
+print a+i*h, b
+#print A, '\n', B, '\n'
+
+X = spalg.solve(A,B)
+X1 = np.dot(spalg.inv(A),B)
+#or np.linalg.inv(A)
+#print X, '\n', U
+
+plt.hold(True)
+plt.plot(np.linspace(a,b,N,endpoint=True),U)
+plt.plot(np.linspace(a,b,N,endpoint=True),X,'+')
+plt.plot(np.linspace(a,b,N,endpoint=True),X1,'--')
+plt.show()
